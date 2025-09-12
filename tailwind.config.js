00b578b9c3a006964491e918ebc39f558b9e5d4c
Micio Dev <micio86dev@gmail.com>
2025-09-12 11:10:24 +0100

    fix: clean firebase

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 const colors = require("tailwindcss/colors");
 
-module.exports = {
+export default {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
