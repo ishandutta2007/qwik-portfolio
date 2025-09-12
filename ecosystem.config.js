bddd6e61732d23802f71ac6c88bdbca7c2d8c75b
Micio Dev <micio86dev@gmail.com>
2025-09-12 09:34:18 +0100

    fix: tieout 60

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,13 +7,6 @@ module.exports = {
       watch: false,
       env: {
         NODE_ENV: "production",
-        VITE_FIREBASE_API_KEY: "AIzaSyAgYHJGQfmVNWB9c5qbejqofavuEXNjTEg",
-        VITE_FIREBASE_APP_ID: "1:923208738119:web:fb48c905ab5548c9005862",
-        VITE_FIREBASE_APP_NAME: "portfolio-a52ee",
-        VITE_FIREBASE_MEASUREMENT_ID: "G-N02R44P8NB",
-        VITE_FIREBASE_MESSAGING_SENDER_ID: "923208738119",
-        VITE_FIREBASE_DB_URL:
-          "https://portfolio-a52ee-default-rtdb.europe-west1.firebasedatabase.app",
       },
       error_file: "./logs/err.log",
       out_file: "./logs/out.log",
@@ -29,7 +22,7 @@ module.exports = {
       repo: "git@github.com:micio86dev/qwik-portfolio.git",
       path: "/var/www/html/micio86",
       "post-deploy":
-        "bun i && bun run build && pm2 reload ecosystem.config.js --only portfolio",
+        "cp .env.example .env && bun i && bun run build && pm2 reload ecosystem.config.js --only portfolio",
       ssh_options: "StrictHostKeyChecking=no",
     },
   },
