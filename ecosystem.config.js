cc60df23ed0ee129bdc19f79cd57062a6372fc0f
Micio Dev <micio86dev@gmail.com>
2025-09-12 09:05:03 +0100

    fix: tieout 60

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,13 +7,13 @@ module.exports = {
       watch: false,
       env: {
         NODE_ENV: "production",
-        VITE_FIREBASE_API_KEY: process.env.VITE_FIREBASE_API_KEY,
-        VITE_FIREBASE_APP_ID: process.env.VITE_FIREBASE_APP_ID,
-        VITE_FIREBASE_APP_NAME: process.env.VITE_FIREBASE_APP_NAME,
-        VITE_FIREBASE_MEASUREMENT_ID: process.env.VITE_FIREBASE_MEASUREMENT_ID,
-        VITE_FIREBASE_MESSAGING_SENDER_ID:
-          process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-        VITE_FIREBASE_DB_URL: process.env.VITE_FIREBASE_DB_URL,
+        VITE_FIREBASE_API_KEY: "AIzaSyAgYHJGQfmVNWB9c5qbejqofavuEXNjTEg",
+        VITE_FIREBASE_APP_ID: "1:923208738119:web:fb48c905ab5548c9005862",
+        VITE_FIREBASE_APP_NAME: "portfolio-a52ee",
+        VITE_FIREBASE_MEASUREMENT_ID: "G-N02R44P8NB",
+        VITE_FIREBASE_MESSAGING_SENDER_ID: "923208738119",
+        VITE_FIREBASE_DB_URL:
+          "https://portfolio-a52ee-default-rtdb.europe-west1.firebasedatabase.app",
       },
       error_file: "./logs/err.log",
       out_file: "./logs/out.log",
